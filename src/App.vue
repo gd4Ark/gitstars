2785461ff9cff819ea8de13c084c73dddd34caf5
4Ark <gd.4ark@gmail.com>
2025-01-25 14:59:06 +0800

    chore: 移除不必要的内容

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <template v-if="userStore.token">
     <Sidebar />
     <div class="flex flex-auto flex-col" style="width: calc(100% - 18rem)">
-      <Header />
       <div class="flex" style="height: calc(100% - 4rem)">
         <SidebarRepository />
         <RepositoryContent />
@@ -14,7 +13,6 @@
 
 <script setup>
 import Sidebar from '@/components/sidebar/index.vue';
-import Header from '@/components/header/index.vue';
 import SidebarRepository from '@/components/sidebar-repository/index.vue';
 import RepositoryContent from '@/components/repository-content/index.vue';
 import Unauth from '@/components/unauth.vue';
--- src/components/sidebar/index.vue
+++ src/components/sidebar/index.vue
@@ -1,41 +1,11 @@
 <template>
   <div class="flex h-full w-72 flex-none flex-col bg-[#28343d] text-[#ddd]">
-    <a :href="BRAND_URI">
-      <h1
-        class="brand-text flex h-16 flex-none items-center justify-center font-['gitstars'] text-3xl font-bold uppercase text-[#948aec]"
-      >
-        <svg-icon name="logo" class="mr-1" />
-        {{ BRAND }}
-      </h1>
-    </a>
-
-    <TagSrcTab />
-
     <TagSrcTabSelf v-show="tagStore.tagSrc === 'star'" />
-    <!-- <TagSrcGithub v-show="tagStore.tagSrc === 'ranking'" /> -->
-
-    <footer
-      class="brand-text h-8 flex-none border-t border-solid border-white/10 text-sm font-bold"
-    >
-      <a
-        :href="userStore.userinfo.html_url"
-        class="flex h-full items-center justify-center"
-        rel="noopener noreferrer"
-      >
-        Author：
-        <h1>
-          {{ userStore.userinfo.html_url.split('/').pop() }}
-          <svg-icon name="share" class="text-xs text-[#76d0a3]" />
-        </h1>
-      </a>
-    </footer>
   </div>
 </template>
 
 <script setup>
-import TagSrcTab from './tag-src-tab.vue';
 import TagSrcTabSelf from './tag-src-self/index.vue';
-import TagSrcGithub from './tag-src-github.vue';
 import { useUserStore } from '@/store/user';
 import { useTagStore } from '@/store/tag';
 import { BRAND, BRAND_URI } from '@/constants';
